2941d6770d4c23a0fe0ceedc3fb73ac52554fe54
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-29 20:33:40 +0530

    REFRACROR: Extending program to find second largest and smallest element after sorting

--- Arrays/SecondMaxAndMin.js
+++ Arrays/SecondMaxAndMin.js
@@ -5,34 +5,23 @@ let i=0;
 //storing array with 10 3digit random numbers
 while (i<10) {
     let ranNum=Math.floor(Math.random()*900+100);
-    numArray.push(ranNum)
-    i++
+    numArray.push(ranNum);
+    i++;
 }
 
 //printing the numbers in array
+console.log("Before Sorting :")
 for(let num of numArray){
-    console.log(num)
+    console.log(num);
 }
 
-let max=numArray[0];
-let secondMax=numArray[0];
-let min=numArray[0];
-let secondMin=numArray[0];
+//sorting the array
+numArray.sort();
 
-//finding second largest and smallest numbers in the array
-for(i=0;i<numArray.length;i++){
-    if(numArray[i]>max){
-        secondMax=max;
-        max=numArray[i];   
-    }else if(numArray[i]>secondMax&&numArray[i]!=max){
-        secondMax=numArray[i];
-    }  
-    if(numArray[i]<min){
-        secondMin=min;
-        min=numArray[i]; 
-    }else if(numArray[i]<secondMin&&numArray[i]!=min){
-        secondMin=numArray[i]
-    }
+console.log("\nAfter Sorting :")
+for(let num of numArray){
+    console.log(num);
 }
-console.log("Second largest number : "+secondMax)
-console.log("Second smallest number : "+secondMin)
+
+console.log("\nSecond largest number : "+numArray[numArray.length-2]);
+console.log("Second smallest number : "+numArray[1]);
