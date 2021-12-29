0d65facfb104d90648d795f11ebf385a764b988b
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-29 21:24:12 +0530

    CREATED program to show sum of three integers add to zero

---
+++ Arrays/SumOfThreeNumAddsToZero.js
@@ -0,0 +1,13 @@
+let arr = [0, -1, 2, -3, 1];
+
+for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+        for (let k = j + 1; k < arr.length; k++) {
+            if (arr[i] + arr[j] + arr[k] === 0)
+            {
+                console.log("("+arr[i]+") + ("+arr[j]+") + ("+arr[k]+") = "+0)
+            }
+        }
+    }
+}
+
